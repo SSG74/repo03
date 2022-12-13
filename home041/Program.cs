c92f86c6ae9fb72d3440a6c8add557c6bbf1b71a
Svutlana <fire_fly_74@mail.ru>
2022-12-13 21:04:47 +0500

    home041

---
+++ home041/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 29. На входе массив из 8 элементов, вывести его на экран
+int [] arr = {1, 2, 5, 7, 19};
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array [i]}, ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+
+
